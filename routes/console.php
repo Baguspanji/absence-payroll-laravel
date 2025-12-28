ee00e4ae3176c05cedf9819e32f01eaab7743265
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2025-12-28 21:06:08 +0700

    refactor: move attendance processing schedule to console routes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Jalankan command ini setiap hari pada pukul 02:00 pagi
+Schedule::call(function () {
+    Artisan::call('app:process-attendance');
+})->dailyAt('02:00');
+
