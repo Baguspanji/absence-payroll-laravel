a1fa8766c2ba4ebce431d468d2cd232cf1d22195
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2026-01-12 15:38:15 +0700

    fix: improve code formatting and consistency in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,19 +6,20 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'log.iclock' => \App\Http\Middleware\LogIclockRequests::class,
-            'is_admin' => \App\Http\Middleware\IsAdmin::class,
-            'is_leader' => \App\Http\Middleware\IsLeader::class,
+            'is_admin'   => \App\Http\Middleware\IsAdmin::class,
+            'is_leader'  => \App\Http\Middleware\IsLeader::class,
         ]);
         $middleware->validateCsrfTokens(except: [
             '/iclock/cdata',
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
